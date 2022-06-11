7f47007396c4459cfa1de249dbce08bfde4bca70
Felipe Palazzo <palazzofelipe@gmail.com>
2022-06-11 18:47:54 +0100

    move to ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
 import React from 'react'
+import Head from 'next/head'
 import Cover from '../components/Cover/Cover'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Paulina | Felipe &hearts;</title>
+        <title>Paulina &amp; Felipe &hearts;</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Cover />
