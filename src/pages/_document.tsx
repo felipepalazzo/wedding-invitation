35716fe3c683b8aea684744a033a3443d1419439
Felipe Palazzo <palazzofelipe@gmail.com>
2022-07-26 19:05:23 +0100

    update some open tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,18 +17,31 @@ export default function Document() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="SAVE THE DATE" />
+        <meta property="og:image:width" content="2048" />
+        <meta property="og:image:height" content="1277" />
         <meta property="og:site_name" content="Paulina &amp; Felipe Wedding" />
         <meta property="og:url" content="http://paulinafelipe.day" />
         <meta
           property="og:description"
-          content="Start packing and get ready: you have been invited to the greatest event of the year. Save the date: 27/05/2023. Location: Artbistro, Stalowa 52 in Warsaw, Poland."
+          content="Start packing and get ready: you have been invited to the greatest event of the year"
         />
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
           content="https://gczfgjjqaugqbtjzaidz.supabase.co/storage/v1/object/sign/img/IMG_3765.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWcvSU1HXzM3NjUuSlBHIiwiaWF0IjoxNjU4NTk5MTU4LCJleHAiOjE5NzM5NTkxNTh9.CDOf4Bx0J35eLRQNA2O8ECbr39n7mNtQtm7qFlRWrvU"
         />
-        <title>Paulina &amp; Felipe </title>
+        <meta name="twitter:title" content="SAVE THE DATE" />
+        <meta
+          name="twitter:description"
+          content="Start packing and get ready: you have been invited to the greatest event of the year"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:image"
+          content="https://gczfgjjqaugqbtjzaidz.supabase.co/storage/v1/object/sign/img/IMG_3765.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWcvSU1HXzM3NjUuSlBHIiwiaWF0IjoxNjU4NTk5MTU4LCJleHAiOjE5NzM5NTkxNTh9.CDOf4Bx0J35eLRQNA2O8ECbr39n7mNtQtm7qFlRWrvU"
+        />
+        <meta name="twitter:url" content="http://paulinafelipe.day" />
+        <title>Paulina &amp; Felipe Wedding</title>
       </Head>
       <body>
         <Main />
